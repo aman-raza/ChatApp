dc1afde2036f45fee38e25756ca45875ffbac66e
aman-raza <amanraza1234@gmail.com>
2021-11-26 21:41:26 +0530

    commit 1

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('We have a new user connection!!!');
+    socket.on('disconnect', () => {
+        console.log('User disconnected!!!');
+    });
+});
+
+app.use(router);
+
+server.listen(PORT, () =>  
+    console.log(`Server running on port ${PORT}`)
+);
