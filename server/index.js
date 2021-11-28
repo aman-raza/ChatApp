35563b752a074db2af7271983a5e04b4c8d08728
aman-raza <amanraza1234@gmail.com>
2021-11-29 00:17:20 +0530

    final alpha

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
 
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -11,17 +13,23 @@ const server = http.createServer(app);
 const io = socket(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new user connection!!!');
-
     socket.on('join',({ name, room }, callback) => {
-        console.log(name, room);
+        const {error, user} = addUser({id: socket.id, name, room});
 
-        const error = true;
+        if(error) return callback({error: 'error'});
 
-        if(error) {
-            return callback({error: 'error'});
-        }
+        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined!`});
 
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
         callback();
     });
 
